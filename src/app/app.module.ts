2da0c29adfd98f1e9df6e54dca309000d6b52b24
Francisco Ernesto Teixeira <fco.ernesto@gmail.com>
2023-04-28 17:34:54 -0300

    Code formating

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,18 +7,14 @@ import {ButtonModule} from 'primeng/button';
 import {RippleModule} from 'primeng/ripple';
 import {InputTextModule} from 'primeng/inputtext';
 import {AutoFocusModule} from 'primeng/autofocus';
-import { SearchComponent } from './pages/search/search.component';
-import {SearchFormComponent} from './pages/search-form/search-form.component';
-import {SearchResultComponent} from './pages/search-result/search-result.component';
+import {SearchComponent} from './pages/search/search.component';
 import {TableModule} from 'primeng/table';
 import {FormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SearchComponent,
-    SearchFormComponent,
-    SearchResultComponent
+    SearchComponent
   ],
   imports: [
     BrowserModule,
