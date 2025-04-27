f2d7a78e43e3229c19a916af6de37020f8d1a150
Francisco Ernesto Teixeira <fco.ernesto@gmail.com>
2025-04-27 03:30:52 -0300

    Add coverage report support

---
+++ karma.conf.js
@@ -0,0 +1,27 @@
+module.exports = function (config) {
+  config.set({
+    basePath: '',
+    frameworks: ['jasmine', '@angular-devkit/build-angular'],
+    plugins: [
+      require('karma-jasmine'),
+      require('karma-chrome-launcher'),
+      require('karma-jasmine-html-reporter'),
+      require('karma-coverage')
+    ],
+    client: {
+      jasmine: {},
+      clearContext: false
+    },
+    reporters: ['progress', 'kjhtml', 'coverage'],
+    coverageReporter: {
+      dir: require('path').join(__dirname, './coverage'),
+      subdir: '.',
+      reporters: [
+        { type: 'html' },
+        { type: 'text-summary' }
+      ]
+    },
+    browsers: ['ChromeHeadless'],
+    restartOnFileChange: true
+  });
+};
